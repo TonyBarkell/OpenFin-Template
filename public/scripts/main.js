1f6aa66259e2fbf5e76cf0c64af8d279e6471ffb
Tony <tonybarkell@gmail.com>
2019-01-03 10:20:32 +0000

    added maximise buton to the tilte bar

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,3 +7,8 @@ function closeWindow(){
     var finWindow = fin.desktop.Window.getCurrent();
     finWindow.close();
 }
+
+function maximiseWindow(){
+    var finWindow = fin.desktop.Window.getCurrent();
+    finWindow.maximize();
+}
